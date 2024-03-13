13c50b2bcc638290f6c1a9e26e1b7130e74ae988
Deise Sales <deisesalles18@gmail.com>
2024-03-12 23:13:28 -0300

    exercicio 03

--- Exercicio002.py
+++ Exercicio002.py
@@ -1,2 +1,3 @@
 mensagem = input('Qual é o seu nome?')
 print('É um prazer te conhecer, {}!'.format(mensagem))
+# a função .format é da string
---
+++ Exercicio003.py
@@ -0,0 +1,4 @@
+x = int(input('Digite o primeiro numero:'))
+y = int(input('Digite o segundo numero:'))
+print('A soma entre os dois numeros é {}'.format(x+y))
+# a função .format é da string
