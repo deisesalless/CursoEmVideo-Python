2c26ebf78bfe8f3653d8ee79c49fad2d4cd2d8c4
Deise Sales <deisesalles18@gmail.com>
2024-03-13 09:17:54 -0300

    exercicio 09

---
+++ Exercicio009.py
@@ -0,0 +1,8 @@
+a = input('Digite algo: ')
+print('O tipo primitivo desse valor é ', type(a))
+print('Tem espaços? ', a.isspace())
+print('É um numero? ', a.isnumeric())
+print('É alfabético? ', a.isalpha())
+print('É alfanumerico? ', a.isalnum())
+print('É menusculo?', a.islower())
+print('É letra maiuscula? ', a.isupper())
