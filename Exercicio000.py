145cfd4e572dae7f8ce12c9109659f408a551510
Deise Sales <deisesalles18@gmail.com>
2024-03-12 22:29:02 -0300

    teste

---
+++ Exercicio000.py
@@ -0,0 +1,16 @@
+print('Hello World')
+
+print('=========== DESAFIO 01 ===========')
+nome = input('Qual é o seu nome?')
+print('Seja bem vindo(a)' , nome)
+
+print('=========== DESAFIO 02 ===========')
+dia = input('Dia = ')
+mes = input('Mês = ')
+ano = input('Ano = ')
+print('Você nasceu no dia',dia,'de',mes,'de',ano,'. Correto?')
+
+# eu converti o input para int
+x = int(input('Digite o primeiro numero:'))
+y = int(input('Digite o segundo numero:'))
+print('A soma entre os dois numeros é', x + y)
