f6fe28da9413b777282f49e4663dad2c3d0e9fb9
Deise Sales <deisesalles18@gmail.com>
2024-03-12 23:29:41 -0300

    exercicio 07

---
+++ Exercicio007.py
@@ -0,0 +1,4 @@
+numero1 = int(input('Digite um numero: '))
+numero2 = int(input('Digite um numero: '))
+soma = numero1 + numero2
+print('A soma entre {0} e {1} é de {2}'.format(numero1, numero2, soma))
